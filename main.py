e1244a623b905f56fa2fbd14c12e01a5697e7a69
Norwood91 <robnorwood91@gmail.com>
2023-12-22 15:34:27 -0800

    Created the barebones structure of the game. Created the overall game class. Installed pygame and sys. Created an instance of the game.

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('Start of a beautiful project!')
+import sys
+
+import pygame
+
+
+class UltimateSaiyan:
+    """Class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game and create game resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('Ultimate Saiyan')
+
+    def run_game(self):
+        """Starts the main loop of the game"""
+        while True:
+            # Watch for keyboard and mouse events
+            # Returns a list of events that have taken place since the last time the run_game function was called
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible. Draws an empty screen on each pass through the while loop,
+            # erasing the old screen so only the new one is visible
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make an instance of the game, and run the game.
+    ui = UltimateSaiyan()
+    ui.run_game()
